70e249cd1c2643a540aced5f4ce26e9b8fd2eb61
Aki Gao <iknowsmtp@gmail.com>
2018-03-25 18:09:27 -0400

    Update add scripts utility to always add Webpack scripts to package.json

--- addScripts.js
+++ addScripts.js
@@ -2,17 +2,15 @@ const fs = require('fs');
 
 const packageJson = require('../package.json');
 
-if (packageJson.scripts === undefined) {
-  console.log('adding Webpack scripts...');
-  packageJson.scripts = {
-    start:
-      'NODE_PATH=$(pwd) nodemon --watch webpack.config.babel.js --watch webpack --exec "webpack-dev-server --mode development --env.NODE_ENV=development"',
-    build:
-      'yarn clean && webpack --progress --colors --mode production --env.NODE_ENV=production',
-    clean: 'rm -rf dist',
-  };
+console.log('adding Webpack scripts...');
+packageJson.scripts = {
+  start:
+    'NODE_PATH=$(pwd) nodemon --watch webpack.config.babel.js --watch webpack --exec "webpack-dev-server --mode development --env.NODE_ENV=development"',
+  build:
+    'yarn clean && webpack --progress --colors --mode production --env.NODE_ENV=production',
+  clean: 'rm -rf dist',
+};
+fs.unlinkSync('../package.json');
+fs.writeFileSync('../package.json', JSON.stringify(packageJson));
 
-  fs.writeFileSync('../package.json', JSON.stringify(packageJson));
-
-  console.log('Webpack scripts added!');
-}
+console.log('Webpack scripts added!');
